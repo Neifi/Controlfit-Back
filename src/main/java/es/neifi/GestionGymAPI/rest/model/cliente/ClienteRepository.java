d1589d9f9f3f2962d4cdf44cd832b18d535ff1d6
Neifi <reytax96@gmail.com>
2020-05-31 01:33:01 +0200

    bug post cliente

--- src/main/java/es/neifi/GestionGymAPI/rest/model/cliente/ClienteRepository.java
+++ src/main/java/es/neifi/GestionGymAPI/rest/model/cliente/ClienteRepository.java
@@ -24,8 +24,8 @@ public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
 	List<Cliente> findAllByOrderByIdAsc();
 
 	
-	@Query(value = "SELECT id_gimnasio FROM cliente INNER JOIN usuario ON cliente.id = usuario.id_usuario WHERE id_usuario = ?", nativeQuery = true)
-	Cliente findIdGimnasioByIdUsuario(int id_usuario);
+	@Query(value = "SELECT id_gimnasio FROM cliente INNER JOIN usuario ON cliente.id = usuario.id_usuario WHERE id_usuario = :id_usuario")
+	Cliente findIdGimnasioByIdUsuario(@Param(value = "id_usuario") int id_usuario);
 	
 	
 	
