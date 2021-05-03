d94bc1d11b285798368e1dfc618a604bfc5311ac
Neifi <neifialcantara@gmail.com>
2021-05-03 22:31:09 +0200

    Migracion a microservicios -> client-service

---
+++ src/main/java/es/neifi/controlfit/cliente/model/PostalAddress.java
@@ -0,0 +1,1 @@
+package es.neifi.controlfit.cliente.model;public class PostalAddress implements java.io.Serializable{private java.lang.String calle;private java.lang.String codigo_postal;private java.lang.String ciudad;private java.lang.String provincia;	public PostalAddress()	{	}}
